78735bf33015215f350281a37a5a844e11587976
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-08-29 13:48:03 -0400

    refactor

--- lipsim/main.py
+++ lipsim/main.py
@@ -132,12 +132,12 @@ if __name__ == '__main__':
 
     parser.add_argument("--account", type=str, default='dci@v100',
                         help="Account to use for slurm.")
-    parser.add_argument("--ngpus", type=int, default=4,
-                        help="Number of GPUs to use.")
+    parser.add_argument("--ngpus", type=int, default=1,
+                        help="Number of GPUs to use.") # todo 4
     parser.add_argument("--nnodes", type=int, default=1,
                         help="Number of nodes.")
-    parser.add_argument("--timeout", type=int, default=1200,
-                        help="Time of the Slurm job in minutes for training.")
+    parser.add_argument("--timeout", type=int, default=60,
+                        help="Time of the Slurm job in minutes for training.") # todo 1200
     parser.add_argument("--partition", type=str, default="gpu_p13",
                         help="Partition to use for Slurm.")
     parser.add_argument("--qos", type=str, default="qos_gpu-t3",
