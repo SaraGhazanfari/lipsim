03895cd5b34b7a4ee749f33d9a4df5f0e611c192
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-09-05 15:06:20 -0400

    bug fixed

--- lipsim/core/data/coco_datast.py
+++ lipsim/core/data/coco_datast.py
@@ -1,44 +1,24 @@
-import os.path
-from typing import Any, Callable, Optional, Tuple
-
-from PIL import Image
+from typing import Callable, Optional
 from torch.utils.data import DataLoader
-from torchvision.datasets import CocoDetection
+from torchvision import datasets
+from torchvision.transforms import transforms
 
 
-class COCODataset(CocoDetection):
+class COCODataset:
 
     def __init__(
-            self, config: Optional, root: str, batch_size: int,
-            annFile='path2json', num_workers=10,
-            transform: Optional[Callable] = None,
-            transforms: Optional[Callable] = None,
-            is_training=True, is_distributed=False, world_size=1
-    ) -> None:
-        super().__init__(root=config.data_dir, transforms=transforms, transform=transform, annFile=annFile)
-        from pycocotools.coco import COCO
-
-        self.coco = COCO(annFile)
-        self.ids = list(sorted(self.coco.imgs.keys()))
+            self, config: Optional, batch_size: int, num_workers=10,
+            is_training=True, is_distributed=False, world_size=1) -> None:
         self.batch_size = batch_size
         self.num_workers = num_workers
-
-    def _load_image(self, id: int) -> Image.Image:
-        path = self.coco.loadImgs(id)[0]["file_name"]
-        return Image.open(os.path.join(self.root, path)).convert("RGB")
-
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
-        id = self.ids[index]
-        image = self._load_image(id)
-
-        if self.transforms is not None:
-            image = self.transforms(image)
-
-        return image
-
-    def __len__(self) -> int:
-        return len(self.ids)
+        self.config = config
+        self.transform = transforms.Compose([
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]),
 
     def get_dataloader(self):
-        return DataLoader(self, batch_size=self.batch_size,
+        dataset = datasets.ImageFolder(self.config.data_dir)
+        return DataLoader(dataset, batch_size=self.batch_size,
                           num_workers=self.num_workers, shuffle=False)
