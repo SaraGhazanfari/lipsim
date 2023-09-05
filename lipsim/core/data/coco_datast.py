5f54f6cdd8555570525d15d852658c3251e09fde
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-09-05 15:18:41 -0400

    bug fixed

--- lipsim/core/data/coco_datast.py
+++ lipsim/core/data/coco_datast.py
@@ -18,7 +18,7 @@ class COCODataset:
             transforms.CenterCrop(224),
             transforms.ToTensor(),
             # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]),
+        ])
 
     def get_dataloader(self):
         dataset = datasets.ImageFolder(self.config.data_dir, transform=self.transform)
