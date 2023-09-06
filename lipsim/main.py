a406bb544f953003f342b5fcf115d49b18686640
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-09-06 16:55:17 -0400

    submitit config changed

--- lipsim/main.py
+++ lipsim/main.py
@@ -130,13 +130,15 @@ def main(config):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train or Evaluate Lipschitz Networks.')
 
-    parser.add_argument("--account", type=str, default='dci@v100',
-                        help="Account to use for slurm.")
+    # parser.add_argument("--account", type=str, default='dci@v100',
+    #                     help="Account to use for slurm.")
+    parser.add_argument("--gres", type=str, default='a100',
+                        help="Number of GPUs to use.")
     parser.add_argument("--ngpus", type=int, default=4,
                         help="Number of GPUs to use.") #
     parser.add_argument("--nnodes", type=int, default=1,
                         help="Number of nodes.")
-    parser.add_argument("--timeout", type=int, default=1440,
+    parser.add_argument("--timeout", type=int, default=30,
                         help="Time of the Slurm job in minutes for training.") # 1440
     parser.add_argument("--partition", type=str, default="gpu_p13",
                         help="Partition to use for Slurm.")
