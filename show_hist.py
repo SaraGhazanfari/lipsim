0cd444a37cb852fd71a1d492dbfec6b17bfa8749
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-09-18 12:49:39 -0400

    slight changes

--- show_hist.py
+++ show_hist.py
@@ -30,5 +30,5 @@ def plot_histogram(dreamsim_list, l2_list, save_path, y_bins_max=250,
 
 if __name__ == '__main__':
     dreamsim_list = torch.load('dists.pt', map_location=torch.device('cpu'))
-    l2_list = torch.load('l2_lists.pt', map_location=torch.device('cpu'))
+    l2_list = torch.load('l2_dists.pt', map_location=torch.device('cpu'))
     plot_histogram(dreamsim_list, l2_list, 'fig.pdf')
