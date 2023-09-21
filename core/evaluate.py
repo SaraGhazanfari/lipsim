c15c6791c8b1cad097208e9149fbccbf2fcd75eb
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-09-21 13:13:21 -0400

    certified accuracy added

--- core/evaluate.py
+++ core/evaluate.py
@@ -156,7 +156,6 @@ class Evaluator:
             fy_fi = (outputs.max(dim=1)[0].reshape(-1, 1) - outputs)
             mask = (outputs.max(dim=1)[0].reshape(-1, 1) - outputs) == 0
             fy_fi[mask] = torch.inf
-            print(fy_fi.shape, bound.shape)
             radius = (fy_fi / bound).min(dim=1)[0]
             for i, eps_float in enumerate(eps_float_list):
                 certified = radius > eps_float
