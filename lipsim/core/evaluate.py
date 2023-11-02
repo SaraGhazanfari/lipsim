d8138c68c883336d33f492923627198554633619
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-11-02 19:08:37 -0400

    refactor

--- lipsim/core/evaluate.py
+++ lipsim/core/evaluate.py
@@ -177,7 +177,7 @@ class Evaluator:
             mask = (outputs.max(dim=1)[0].reshape(-1, 1) - outputs) == 0
             fy_fi[mask] = torch.inf
             radius = (fy_fi / bound).min(dim=1)[0]
-            print(radius)
+
             for i, eps_float in enumerate(eps_float_list):
                 certified = radius > eps_float
                 running_certified[i] += torch.sum(correct & certified).item()
