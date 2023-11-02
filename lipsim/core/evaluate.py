de6f4c7a9960c1a7a76baaf151c81c01c9ef7d25
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-11-02 15:51:34 -0400

    refactor

--- lipsim/core/evaluate.py
+++ lipsim/core/evaluate.py
@@ -336,7 +336,7 @@ class Evaluator:
         for idx, norm_value in enumerate(embed_ref_norm):
             if norm_value < 1.0:
                 embed_ref[idx] += (2 / embed_ref.shape[1]) * torch.ones_like(embed_ref[idx])
-                print(embed_ref.shape[1], torch.ones_like(embed_ref[idx]).shape, torch.norm(embed_ref[idx], p=2))
+                print(embed_ref.shape[1], torch.norm(torch.ones_like(embed_ref[idx]),p=2), torch.norm(embed_ref[idx], p=2))
                 time.sleep(2)
         return embed_ref
 
