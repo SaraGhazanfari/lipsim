f56781171d86ba04fec0395fb57f8f04ad13d26f
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-11-03 21:14:11 -0400

    bug fixed

--- lipsim/core/evaluate.py
+++ lipsim/core/evaluate.py
@@ -308,7 +308,7 @@ class Evaluator:
 
     def model_wrapper(self, img_left, img_right):
         def metric_model(img):
-            if len(img.shape) <= 4:
+            if len(img.shape) > 4:
                 img_ref, img_0, img_1 = img[:, 0, :, :].squeeze(1), img[:, 1, :, :].squeeze(1), img[:, 2, :, :].squeeze(
                     1)
             else:
