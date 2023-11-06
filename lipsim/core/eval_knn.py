b26b599a52a9aa60b76489bfd52bb9dd922f98c0
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-11-06 17:34:33 -0500

    main updated

--- lipsim/core/eval_knn.py
+++ lipsim/core/eval_knn.py
@@ -51,7 +51,7 @@ class KNNEval:
                 self.model, device_ids=[self.local_rank], output_device=self.local_rank)
         else:
             self.model = nn.DataParallel(self.model, device_ids=range(torch.cuda.device_count()))
-
+            utils.setup_distributed_training(self.world_size, self.rank)
     def _load_dataloader(self):
         transform = transforms.Compose([
             transforms.Resize(256, interpolation=3),
