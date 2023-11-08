190336d4adc6e3a69ea455aee4cf9aae5cf4ee97
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-11-07 23:00:51 -0500

    distributed training is off for a min

--- lipsim/core/trainer.py
+++ lipsim/core/trainer.py
@@ -15,11 +15,11 @@ from torch import nn
 from torch.nn.parallel import DistributedDataParallel
 from torch.distributed.elastic.multiprocessing.errors import record
 from tqdm import tqdm
-
+from dreamsim.model import download_weights, dreamsim
 from lipsim.core import utils
 from lipsim.core.data.night_dataset import NightDataset
 from lipsim.core.data.readers import readers_config
-from lipsim.core.models.dreamsim.model import dreamsim
+
 from lipsim.core.models.l2_lip.model import NormalizedModel, L2LipschitzNetwork, PerceptualMetric
 
 # from core.models.dreamsim.model import dreamsim
@@ -77,11 +77,11 @@ class Trainer:
         self.train_dir = self.config.train_dir
         self.ngpus = torch.cuda.device_count()
 
-        #todo job_env = submitit.JobEnvironment()
-        self.rank = 0#todo int(job_env.global_rank)
-        self.local_rank = 0#todo int(job_env.local_rank)
-        self.num_nodes = 1#todo int(job_env.num_nodes)
-        self.num_tasks = 1#todo int(job_env.num_tasks)
+        # todo job_env = submitit.JobEnvironment()
+        self.rank = 0  # todo int(job_env.global_rank)
+        self.local_rank = 0  # todo int(job_env.local_rank)
+        self.num_nodes = 1  # todo int(job_env.num_nodes)
+        self.num_tasks = 1  # todo int(job_env.num_tasks)
         self.is_master = bool(self.rank == 0)
 
         # Setup logging
@@ -143,9 +143,9 @@ class Trainer:
         if self.local_rank == 0:
             logging.info(f'Number of parameters to train: {param_size}')
 
-        self.teacher_model, _ = dreamsim(pretrained=True,
-                                         dreamsim_type=self.config.teacher_model_name,
-                                         cache_dir=self.config.dreamsim_path)
+        download_weights(cache_dir='./checkpoints', dreamsim_type=self.config.teacher_model_name)
+        self.dreamsim_model, _ = dreamsim(pretrained=True, dreamsim_type=self.config.teacher_model_name,
+                                          cache_dir='./checkpoints')
         self.teacher_model = self.teacher_model.cuda()
 
         # setup distributed process if training is distributed
