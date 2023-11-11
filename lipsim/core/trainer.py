88789c788811a9b8ba80cd1913ed3bd7122fb424
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-11-11 14:19:49 -0500

    debug

--- lipsim/core/trainer.py
+++ lipsim/core/trainer.py
@@ -132,7 +132,7 @@ class Trainer:
             self.world_size, self.global_batch_size))
 
         torch.cuda.set_device(self.local_rank)
-
+        print(self.config.dataset)
         # load dataset
         Reader = readers_config[self.config.dataset]
         self.reader = Reader(config=self.config, batch_size=self.batch_size, is_training=True,
