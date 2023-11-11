c0f79ca9c4f1922e23a911bbea8a2f7df46bf3ee
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-11-11 13:02:04 -0500

    debug

--- lipsim/core/evaluate.py
+++ lipsim/core/evaluate.py
@@ -325,7 +325,6 @@ class Evaluator:
         if self.config.attack:
             img_ref = self.general_attack.generate_attack(img_ref, img_left, img_right, target,
                                                           target_model=self.model_wrapper(img_left, img_right))[0]
-        print(img_ref.shape)
         dist_0, dist_1, _ = self.perceptual_metric.get_cosine_score_between_images(img_ref, img_left, img_right)
         if len(dist_0.shape) < 1:
             dist_0 = dist_0.unsqueeze(0)
