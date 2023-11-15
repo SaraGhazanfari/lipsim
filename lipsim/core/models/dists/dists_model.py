d478228e5536845332f54143af786beda29bb267
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-11-15 09:47:12 -0500

    refactor

--- lipsim/core/models/dists/dists_model.py
+++ lipsim/core/models/dists/dists_model.py
@@ -63,6 +63,7 @@ class DISTS(torch.nn.Module):
         self.beta.data.normal_(0.1, 0.01)
         if load_weights:
             weights = torch.load(os.path.join(os.getcwd(), 'lipsim/core/models/dists','weights.pt'))
+            print(weights)
             self.alpha.data = weights['alpha']
             self.beta.data = weights['beta']
 
