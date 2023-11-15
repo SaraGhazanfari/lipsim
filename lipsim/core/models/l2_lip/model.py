eebb6f787964a910293e931cfa4574ba1572b84c
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-11-15 00:52:22 -0500

    refactor

--- lipsim/core/models/l2_lip/model.py
+++ lipsim/core/models/l2_lip/model.py
@@ -92,8 +92,8 @@ class LPIPSMetric:
         dist_0 = self.lpips_metric(img_ref, img_left)
         dist_1 = self.lpips_metric(img_ref, img_right)
         if not requires_grad:
-            dist_0 = dist_0.detach()
-            dist_1 = dist_1.detach()
+            dist_0 = dist_0.detach().squeeze()
+            dist_1 = dist_1.detach().squeeze()
         return dist_0, dist_1, None
 
 
