d46ad1cbbb5c7ee1f8577c46c184f4a7e4e080f7
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-11-27 18:12:36 -0500

    distributed setup changed

--- lipsim/core/utils.py
+++ lipsim/core/utils.py
@@ -159,11 +159,12 @@ def setup_distributed_training(world_size, rank):
         if var.upper() in os.environ:
             del os.environ[var.upper()]
     # get distributed url
-    # cmd = 'scontrol show hostnames ' + os.getenv('SLURM_JOB_NODELIST')
-    # stdout = subprocess.check_output(cmd.split())
-    # host_name = stdout.decode().splitlines()[0]
-    import platform
-    host_name = platform.node()
+    cmd = 'scontrol show hostnames ' + os.getenv('SLURM_JOB_NODELIST')
+    stdout = subprocess.check_output(cmd.split())
+    host_name = stdout.decode().splitlines()[0]
+    # import platform
+    # host_name = platform.node()
+
     dist_url = f'tcp://{host_name}:9000'
     # setup dist.init_process_group
     # shared_folder = os.environ.get('folder_path')
