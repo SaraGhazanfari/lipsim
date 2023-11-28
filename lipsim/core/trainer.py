432390f3bf29455f6eb1235e980ebca40867ff5b
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-11-28 12:24:31 -0500

    lr scheduler changed

--- lipsim/core/trainer.py
+++ lipsim/core/trainer.py
@@ -194,7 +194,7 @@ class Trainer:
         # define learning rate scheduler
         num_steps = self.config.epochs * (self.reader.n_train_files // self.global_batch_size)
         self.scheduler = utils.cosine_scheduler(
-            base_value=self.config.lr * (self.batch_size * utils.get_world_size()) / 256.,  # linear scaling rule
+            base_value=self.config.lr * 4.,  # linear scaling rule
             final_value=self.config.min_lr,
             epochs=self.config.epochs, niter_per_ep=len(data_loader),
             warmup_epochs=self.config.warmup_epochs,
