1897c7a50154d464841dc1f2c868b7eb5659420a
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-12-05 14:25:53 -0500

    add two part loss

--- lipsim/core/models/l2_lip/model_v2.py
+++ lipsim/core/models/l2_lip/model_v2.py
@@ -67,10 +67,10 @@ class L2LipschitzNetworkPlusProjector(nn.Module):
         layers.append(SDPLin(hidden_dim, bottleneck_dim))
         self.projector = nn.Sequential(*layers)
         # self.apply(self._init_weights)
-        self.last_linear_layer = SDPLin(bottleneck_dim, out_dim, bias=False)
-        self.last_layer = nn.utils.weight_norm(self.last_linear_layer)
-        self.last_layer.weight_g.data.fill_(1)
-        self.last_layer.weight_g.requires_grad = False
+        self.last_layer = SDPLin(bottleneck_dim, out_dim,
+                                 bias=False)  # nn.utils.weight_norm(SDPLin(bottleneck_dim, out_dim, bias=False))
+        # self.last_layer.weight_g.data.fill_(1)
+        # self.last_layer.weight_g.requires_grad = False
         logging.info(f'LipSim: number of parameters for backbone: {utils.get_parameter_number(self.backbone)}')
         logging.info(
             f'LipSim: number of parameters for projector: {utils.get_parameter_number(self.projector) + utils.get_parameter_number(self.last_layer)}')
@@ -85,7 +85,5 @@ class L2LipschitzNetworkPlusProjector(nn.Module):
         embedding = self.backbone(x)
         x = self.projector(embedding)
         x = nn.functional.normalize(x, dim=-1, p=2)
-        print(f'Norm before the weight normalization: {torch.norm(self.last_linear_layer.weight, p=2, dim=1)}')
         x = self.last_layer(x)
-        print(f'Norm after the weight normalization: {torch.norm(self.last_linear_layer.weight, p=2, dim=1)}')
-        return x  # embedding, x
+        return embedding, x
