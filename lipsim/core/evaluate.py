2bfd971043551b128684ad45c1d535498ebb0b66
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-12-04 23:10:16 -0500

    bug fixed

--- lipsim/core/evaluate.py
+++ lipsim/core/evaluate.py
@@ -74,7 +74,7 @@ class Evaluator:
         for k, v in checkpoint['model_state_dict'].items():
             if 'alpha' not in k:
                 if k.startswith('module.backbone'):
-                    new_checkpoint[k.replace('module.backbone.', '')] = v
+                    new_checkpoint[k.replace('module.backbone.', 'module.')] = v
                 new_checkpoint[k] = v
 
         msg = self.model.load_state_dict(new_checkpoint, strict=False)
