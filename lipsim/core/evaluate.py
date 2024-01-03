4aec54fb8d066734bc440c7feb9aff301badcdb9
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-01-02 20:02:28 -0500

    refactor finetune

--- lipsim/core/evaluate.py
+++ lipsim/core/evaluate.py
@@ -331,6 +331,7 @@ class Evaluator:
 
     def model_wrapper(self, img_left, img_right):
         def metric_model(img):
+            print(img.shape)
             if len(img.shape) > 4:
                 img_ref, img_0, img_1 = img[:, 0, :, :, :].squeeze(1), img[:, 1, :, :, :].squeeze(1), img[:, 2, :, :,
                                                                                                       :].squeeze(1)
